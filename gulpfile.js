5cf1005122a0e56dfe1f1d95dbb8bfc8604049ac
Alexandr <net_box@ukr.net>
2021-06-24 15:02:15 +0300

    section portfolio: mixitup, fancybox

--- gulpfile.js
+++ gulpfile.js
@@ -50,6 +50,7 @@ function scripts(){
     return src([
         'node_modules/jquery/dist/jquery.js',
         'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
