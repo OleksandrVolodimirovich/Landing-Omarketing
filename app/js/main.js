d3897961ff4f493ebed911a5e0c2b926c7ca9991
Alexandr <net_box@ukr.net>
2021-07-03 23:19:00 +0300

    @media: header, mob-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,14 @@
 $(function(){
-    var mixer = mixitup('.portfolio__content');
 
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="ico: arrow-left"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="ico: arrow-right"></button>'
     });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
+    var mixer = mixitup('.portfolio__content');
 })
