c57d68f9caaf480c9e5cc2dc073bd67cab0be5e4
Alexandr <net_box@ukr.net>
2021-07-10 15:59:26 +0300

    @media: slider-blog, team, pricing, clients

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,18 @@ $(function(){
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="ico: arrow-left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="ico: arrow-right"></button>'
-    });
+        nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="ico: arrow-right"></button>',
+    
+    responsive:[
+        {
+            breakpoint: 769,
+            settings:{
+                arrows: false
+            }
+        },
+    ]
+});
+
 
     $('.menu__btn').on('click', function(){
         $('.header__top-inner').toggleClass('header__top-inner--active');
