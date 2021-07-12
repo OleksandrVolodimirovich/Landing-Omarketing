aa22af4374b3499f1d7aafa95ee82426827df751
Alexandr <net_box@ukr.net>
2021-07-12 18:55:10 +0300

    none

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
+    $(".menu a").on("click", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор блока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+
     $('.slider-blog__inner').slick({
         dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="ico: arrow-left"></button>',
