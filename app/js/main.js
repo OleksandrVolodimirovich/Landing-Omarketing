f4775ea094f41accc1716d42b7e1917235501932
Alexandr <net_box@ukr.net>
2021-07-21 23:54:57 +0300

    menu fixid

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,10 @@
 $(function(){
 
-    $(".menu a").on("click", function (event) {
-		//отменяем стандартную обработку нажатия по ссылке
+    $(".menu a, .footer__go-top").on("click", function (event) {
 		event.preventDefault();
-
-		//забираем идентификатор блока с атрибута href
 		var id  = $(this).attr('href'),
-
-		//узнаем высоту от начала страницы до блока на который ссылается якорь
 			top = $(id).offset().top;
-		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
+		$('body,html').animate({scrollTop: top}, 2000);
 	});
 
 
@@ -31,7 +24,7 @@ $(function(){
 });
 
 
-    $('.menu__btn').on('click', function(){
+    $('.menu__btn, .menu a').on('click', function(){
         $('.header__top-inner').toggleClass('header__top-inner--active');
     });
 
